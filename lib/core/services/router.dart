e58d6844fb9702b1c088d9366094b960bddf4d24
Ahmad Faisal <ahmad.faisalewy@gmail.com>
2024-01-22 09:59:26 +0700

    feat(on_boarding): injected cubit on the route

--- lib/core/services/router.dart
+++ lib/core/services/router.dart
@@ -1,12 +1,18 @@
 import 'package:education_app/core/common/view/page_under_construction.dart';
+import 'package:education_app/core/services/injection.dart';
+import 'package:education_app/src/on_boarding/presentation/cubit/on_boarding_cubit.dart';
 import 'package:education_app/src/on_boarding/presentation/views/on_boarding_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 Route<dynamic> generateRoute(RouteSettings settings) {
   switch (settings.name) {
     case OnBoardingScreen.routeName:
       return _pageBuilder(
-        (_) => const OnBoardingScreen(),
+        (_) => BlocProvider(
+          create: (context) => sl<OnBoardingCubit>(),
+          child: const OnBoardingScreen(),
+        ),
         settings: settings,
       );
     default:
